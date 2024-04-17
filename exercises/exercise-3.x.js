ee14b8eaad68f2f7150e7e4618e5cebcadcafb18
PhillipMagos <phillipmagos@gmail.com>
2024-04-16 19:56:06 -0700

    ch 3 exercises

---
+++ exercises/exercise-3.x.js
@@ -0,0 +1,139 @@
+// 3.1
+// let arr = ["Milk", "Bread", "Apples"]
+// console.log(arr.length)
+// arr[1] = "Bananas"
+// console.log(arr)
+
+//3.2
+// let arr = [];
+// arr.push("Milk", "Bread", "Apples")
+// arr.splice(1,1, "Bananas", "Eggs")
+// arr.sort()
+// let a = arr.indexOf("Milk")
+// console.log(a)
+// arr.splice(1, 0, "Carrots", "Lettuce")
+// arr.splice(1,0, "Carrots")
+// console.log(arr)
+// let arr2 = ["Juice", "Pop"]
+// let arr3 = arr.concat(arr2)
+// console.log(arr3)
+// arr3.lastIndexOf("Pop")
+
+// //corrections
+// let arr4 = arr3.concat(arr2)
+// console.log(arr4)
+
+//3.3
+// let arr = [1,2,3]
+// let arr2 = [arr, arr, arr];
+// console.log(arr2)
+// console.log(arr2[0][1]);
+// console.log(arr2[1][1]);
+// console.log(arr2[2][1]);
+
+//3.4
+// let myCar = { make: 'honda',
+//         model: 'accord',
+//         stock: 4,
+//         gas: true
+//         };
+
+// let propColor = "blue";
+
+// 3.2
+// let arr1 = ["Milk", "Bread", "Apples"];
+// arr1[1] = "Bananas";
+// console.log(arr1)
+// let tossApples = arr1.pop();
+// console.log(tossApples);
+// arr1.sort();
+// console.log(arr1);
+// let six = arr1.indexOf("Milk");
+// console.log(six);
+// let seven = arr1.splice(1, 0, "Carrots", "Letuce")
+// console.log("prob7 is " + arr1);
+// let newArr = ["Juice", "Pop"];
+// let conbinedArr = arr1.concat(newArr);
+// console.log(conbinedArr)
+// let ten = conbinedArr.pop();
+// console.log(ten);
+
+//3.3
+// let arr1 = [1,2,3];
+// let arr2 = [arr1, arr1, arr1];
+// console.log(arr2)
+// let output = arr2[2][1];
+// console.log(output);
+
+//3.4
+// let myCar = {
+//         make: "kia",
+//         model: "forte",
+//         color: "grey",
+//         wheels: 4,
+//         chickMagnent: "No",
+//         reliable: true
+// };
+// let carColor = "color";
+// console.log(carColor);
+// //2
+// myCar[carColor] = "red";
+// console.log(myCar);
+// //3
+// carColor = "forSale";
+// myCar[carColor] = true
+// console.log(carColor)
+// //4
+// console.log(myCar.make, myCar.model)
+// //5
+// console.log(myCar.forSale)
+
+
+//3.5 
+// let people = {friends: []}
+// let friendOne = {
+//                 fName: "Anil", 
+//                 lName: "Naidu", 
+//                 id: 1234
+//         }
+// let friend2 = {
+//                 fName: "Felipe", 
+//                 lName: "Lopez",
+//                 id: 4321
+//         }
+// let friend3 = {
+//                 fName: "Lax",
+//                 lName: "Mura...",
+//                 id: 8493
+//         }
+// people.friends.push(friendOne, friend2, friend3)
+// console.log(people)
+
+
+// project 1
+
+// const theList = ['Laurence', 'Svekis', true, 35, null, undefined, {test: 'one', score: 55}, ['one', 'two']];
+// //1
+// let one = theList.shift();
+// let one1 = theList.pop();
+// console.log(one, one1);
+// //console.log(theList);
+// //2
+// theList.unshift("FIRST")
+// console.log(theList);
+// //3
+// let three = theList.splice(3,1,"hello World");
+// console.log(theList)
+// //4
+// let four = theList.splice(2,1,"MIDDLE");
+// console.log(theList);
+// //5
+// let five = theList.push("LAST");
+// console.log(theList)
+
+// project 2
+let arr1 = ['ketchup', 'soap', 'mustard', 'calendars', 'garlic bread']
+
+let ketchup = {
+        
+}
